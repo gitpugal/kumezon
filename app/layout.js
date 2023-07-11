f3b49b6985345c5aee587a282d8c9af68c3e3d6f
gitpugal <45727933+gitpugal@users.noreply.github.com>
2023-07-11 22:30:43 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -7,10 +7,10 @@ import ScrollToTopButton from '@/components/ScrollToTopButton';
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata = {
-  title: 'Kumezon.in',
-  description: 'Kumezon.in buy products here for free.',
-}
+// export const metadata = {
+//   title: 'Kumezon.in',
+//   description: 'Kumezon.in buy products here for free.',
+// }
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
